7f6e1ee14a9db434c80f723235dbd768fe1d9f88
Vladislav <wlladisla@gmail.com>
2022-11-05 13:25:04 +0300

    feat: add task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.rm(
+  '04-copy-directory/files-copy',
+  { force: true, recursive: true },
+  (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      copyDir();
+    }
+  }
+);
+
+function copyDir() {
+  fs.mkdir('04-copy-directory/files-copy', { recursive: true }, (err) => {
+    if (err) throw err; // не удалось удалить папку
+    console.log('Папка успешно создана');
+  });
+
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach((file) => {
+        fs.copyFile(
+          path.join(__dirname, `/files/${file}`),
+          path.join(__dirname, `/files-copy/${file}`),
+          (err) => {
+            if (err) {
+              console.log('Error Found:', err);
+            } else {
+              console.log('file add ');
+            }
+          }
+        );
+      });
+    }
+  });
+}
